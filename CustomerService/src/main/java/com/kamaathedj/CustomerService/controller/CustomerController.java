d4fefecd9892646da60448b6886ce37e1720001b
kamaathedj <kamaudavikiruku@gmail.com>
2021-08-06 10:14:47 +0300

    :pencil: Added the user service

--- CustomerService/src/main/java/com/kamaathedj/CustomerService/controller/CustomerController.java
+++ CustomerService/src/main/java/com/kamaathedj/CustomerService/controller/CustomerController.java
@@ -5,6 +5,7 @@ import com.kamaathedj.CustomerService.utilities.Util;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 

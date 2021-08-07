8d0d35df813275fc4043249dda673174917eeca7
kamaathedj <kamaudavikiruku@gmail.com>
2021-08-07 12:11:36 +0300

    minor change

--- CustomerService/src/main/java/com/kamaathedj/CustomerService/CustomerServiceApplication.java
+++ CustomerService/src/main/java/com/kamaathedj/CustomerService/CustomerServiceApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(CustomerServiceApplication.class, args);
 	}
 
